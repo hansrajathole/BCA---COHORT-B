938feebfbffaa81d24090ec88fabbd273ce02756
hansrajathole <hansrajathole1@gmail.com>
2025-08-05 12:45:13 +0530

    Add initial HTML structure and JavaScript file with array manipulation example

---
+++ LEC 1 JS/script.js
@@ -0,0 +1,20 @@
+// let a = 20 
+// let copy = a
+// copy = 40
+
+// console.log(a);
+// console.log(copy);
+
+
+let arr = [10,20,30,40]
+
+
+let copyArr = arr
+arr.push(50)
+console.log('original :',arr);
+console.log("copy arr ", copyArr);
+
+
+
+
+
