d9a55ec3de9a254bd819f74b6f984310c3c46a1b
hansrajathole <hansrajathole1@gmail.com>
2025-08-07 10:27:40 +0530

    Add initial HTML and JavaScript files with array manipulation examples

---
+++ LEC 3 JS/script.js
@@ -0,0 +1,60 @@
+let arr = [1,2,3,4,5,6,7,8]
+
+// Map Method
+// const newArr = arr.map((elem , index)=>{
+//     return elem+5
+// })
+
+// console.log(newArr);
+// console.log(arr);
+
+
+// Filter 
+// let filteredArr = arr.filter((elem , index)=>{
+//     if(elem % 2 === 0){
+//         return elem
+//     }
+// })
+
+// console.log(filteredArr);
+
+
+// let sum = arr.reduce((prev , current)=>{
+//     console.log("prev :", prev , " current :", current);
+//     return prev + current
+// })
+
+// console.log(sum);
+
+// prev      current
+//  1          2     ==== 3
+//  3          3     ==== 6
+//  6          4     ==== 10
+//  10         5     ==== 15
+//  15         6     ==== 21
+//  21         8     ==== 29
+
+
+// arr.forEach((elem , index)=>{
+//     console.log("element :", elem, " ===>", index);
+    
+// })
+
+
+
+// let person = {
+//     "full name" : "raj",
+//     age : 22,
+//     branch : "BCA",
+//     address : {
+//         state : "maharashtra",
+//         city : "Nagpur"
+//     }
+// }
+
+
+// console.log(person.full name );
+// console.log(person["full name"]);
+
+
+
