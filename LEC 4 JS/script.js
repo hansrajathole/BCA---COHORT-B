0ff200b25d7e9af34c91717a64bbd358ad291276
hansrajathole <hansrajathole1@gmail.com>
2025-08-08 10:35:17 +0530

    class 4 done

---
+++ LEC 4 JS/script.js
@@ -0,0 +1,84 @@
+// const person = {
+//     name : "raj",
+//     age : 24,
+//     branch : "BCA",
+//     address : {
+//         state : "MP",
+//         city : "indore",
+//         pin : 444489,
+//     },
+//     fun() {
+//         console.log("hello from", this.name);
+//     }
+
+
+// }
+// person.fun()
+// console.log(person.fun();
+
+
+// person.age = 22
+// let key = "full name"
+// console.log(person[key]);
+
+
+// person.gender = "Male"
+// person.address.state = "MH"
+// console.log(person);
+
+
+
+
+// console.log(this);
+
+
+
+
+// Math Methods
+// Math.floor
+
+
+// console.log(Math.floor(5/3));
+// console.log(7/3);
+
+// console.log(Math.ceil(7/3));
+// let num = Math.random()*10
+
+// // console.log(num);
+
+// let random = Math.floor(num+5)
+
+// console.log(random);
+
+
+// console.log(Math.max(10,30)); // 30
+// console.log(Math.min(10,30)); // 10
+
+// let name = `raju ${10+20}`
+
+// let str = `hello from ${name}`
+
+// console.log(str);
+
+
+
+
+
+// Destructuring (Objects)
+
+// const obj = {
+//     name : "aryan",
+//     age : 25,
+//     mobile : 12345678987654
+
+// }
+
+// let {mobile} = obj
+
+// console.log(mobile);
+// console.log(name);
+// console.log(name);
+// console.log(name);
+// console.log(name);
+
+
