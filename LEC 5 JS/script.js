085ad686b3bdbda2d8253258617ec1bade46e07d
hansrajathole <hansrajathole1@gmail.com>
2025-08-12 18:38:53 +0530

    revision and callback

---
+++ LEC 5 JS/script.js
@@ -0,0 +1,38 @@
+let arr = [10,20,30,40]
+
+
+
+// for(const chacha of arr){
+//     console.log(chacha);   
+// }
+
+
+// let obj = {
+//     name : "kuch bhi",
+//     age : 69,
+//     mo : 98742439875
+// }
+
+// for (const key in obj) {
+//   console.log(key ,"===>",obj[key]);
+// }
+
+
+
+// CallBack Function 
+
+function sum(a,b) {
+   
+    return a+b
+    
+}
+
+function fun(sum) {
+    let ans = sum(10 , 20)
+    console.log(ans);
+    
+    console.log("hello bhaiyo kaise ho");
+
+}
+
+fun(sum)
