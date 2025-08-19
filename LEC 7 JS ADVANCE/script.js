0d622ac5745abd0d45cdefc7700b59a2d6bbf900
hansrajathole <hansrajathole1@gmail.com>
2025-08-19 16:35:21 +0530

    js done

---
+++ LEC 7 JS ADVANCE/script.js
@@ -0,0 +1,81 @@
+// let promise = new Promise((resolve , reject)=>{
+
+//     setTimeout(()=>{
+//         let num = Math.floor(Math.random()*2)
+
+//         if(num === 1){
+//             console.log(num);
+
+//             resolve("successfull")
+//         }else {
+//             console.log(num);
+
+//             reject("rejected")
+//         }
+
+//     },2000)
+
+// })
+
+// // console.log(promise);
+
+// promise.then((res)=>{
+//     console.log(res);
+
+// })
+// .catch((err)=>{
+//     console.log(err);
+
+// })
+
+
+
+// async function getData() {
+//   let url = "https://dummyjso.com/users";
+//   try {
+//     const response = await fetch(url);
+
+//     const result = await response.json();
+//     console.log(result);
+
+//   } catch (error) {
+//     console.log("kuch error aaya",error);
+//   }
+// }
+
+// getData();
+
+
+// localStorage.setItem("name", "pooja")
+// localStorage.setItem("age", 20)
+// localStorage.removeItem("name")
+
+// localStorage.clear()
+
+
+// console.log(a);
+// console.log(b);
+
+
+
+// let a = 10
+// var b = 20
+
+// // console.log(a+b);
+
+// function fun() {
+//     let name = "raju"
+//     let age = 50
+
+//     console.log(name);
+    
+// }
+
+
+
+let a 
+// console.log(a);
+
+
+console.log("hello"*5);
+
