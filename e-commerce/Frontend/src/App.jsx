9d0ab4d04a995bbee8a6d41c8b66308b509dd40d
hansrajathole <hansrajathole1@gmail.com>
2025-10-04 12:18:38 +0530

    feat: initialize frontend with React, Vite, and Tailwind CSS
    - Added package.json for project dependencies and scripts
    - Created Vite configuration file for React and Tailwind CSS integration
    - Implemented main application entry point (main.jsx) with React StrictMode
    - Developed basic App component with a "hello world" message styled with Tailwind CSS
    - Included global CSS file to import Tailwind styles
    - Added SVG assets for Vite and React logos

---
+++ e-commerce/Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-2xl text-red-300 bg-amber-600  '>
+        hello world
+      </h1>
+    </div>
+  )
+}
+
+export default App
